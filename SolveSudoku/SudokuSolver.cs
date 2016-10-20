fbc59fd45b0eb05d0a92bde6a1090fc5c8b6fe30
Rick Qing Xu <qinx@microsoft.com>
2016-10-20 14:17:55 -0700

    Fixed a major missing rule.

--- SolveSudoku/SudokuSolver.cs
+++ SolveSudoku/SudokuSolver.cs
@@ -12,6 +12,7 @@ namespace SolveSudoku
     {
         private List<int[,]> answers = new List<int[,]>();
         private int[,] x;
+        private List<int>[,] squares = new List<int>[3, 3];
         List<int>[] row = new List<int>[9];
         List<int>[] col = new List<int>[9];
 
@@ -21,7 +22,14 @@ namespace SolveSudoku
             for(int i = 0; i < 9; i++)
             {
                 points[i] = i + 1;
+                squares[i / 3, i % 3] = new List<int>();
             }
+
+            for (int j = 0; j < 9; j++)
+            {
+                squares[j / 3, j % 3].AddRange(points);
+            }
+
 
             for(int i = 0; i< 9; i++)
             {
@@ -39,6 +47,8 @@ namespace SolveSudoku
                                 temp[l] = 0;
                             }
                         }
+
+                        squares[i / 3, j / 3].Remove(x[i, j]);
                     }
                 }
                 
@@ -84,7 +94,7 @@ namespace SolveSudoku
         {
             x = problem;
             InitializeData();
-            SolveInternal(row, col, problem);
+            SolveInternal(row, col, squares, problem);
 
             if(answers.Count > 0)
             {
@@ -116,7 +126,36 @@ namespace SolveSudoku
             return new ReadOnlyCollection<int[,]>(answers);
         }
 
-        private int[,] SolveInternal(List<int>[] r, List<int>[] c, int[,] problem)
+        List<int>[] CloneArrays(List<int>[] arr)
+        {
+            var r = new List<int>[arr.Length];
+
+            for (int l = 0; l < 9; l++)
+            {
+                r[l] = new List<int>();
+                r[l].AddRange(arr[l]);
+            }
+
+            return r;
+        }
+
+        List<int>[,] CloneSquares(List<int>[,] sqs)
+        {
+            var r = new List<int>[3, 3];
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    r[i, j] = new List<int>();
+                    r[i, j].AddRange(sqs[i, j]);
+                }
+            }
+
+            return r;
+        }
+
+        private int[,] SolveInternal(List<int>[] r, List<int>[] c, List<int>[,] sqs, int[,] problem)
         {
             for(int i = 0; i < 9; i++)
             {
@@ -124,34 +163,23 @@ namespace SolveSudoku
                 {
                     if(problem[i, j] == 0)
                     {
-                        var choices = r[i].Intersect(c[j]).ToList();
+                        var choices = r[i].Intersect(c[j]).Intersect(sqs[i / 3, j / 3]).ToList();
 
                         if(choices.Any())
                         {
-                            var nr = new List<int>[9];
-                            var nc = new List<int>[9];
-
-                            for (int l = 0; l < 9; l++)
-                            {
-                                nr[l] = new List<int>();
-                                nr[l].AddRange(r[l]);
-                            }
-
-                            for (int l = 0; l < 9; l++)
-                            {
-                                nc[l] = new List<int>();
-                                nc[l].AddRange(c[l]);
-                            }
-
+                            var nr = CloneArrays(r);
+                            var nc = CloneArrays(c);
+                            var nsqs = CloneSquares(sqs);
                             var p = problem.Clone() as int[,];
 
                             foreach(int k in choices)
                             {
                                 nr[i].Remove(k);
                                 nc[j].Remove(k);
+                                nsqs[i / 3, j / 3].Remove(k);
                                 p[i, j] = k;
 
-                                var sub = SolveInternal(nr, nc, p);
+                                var sub = SolveInternal(nr, nc, nsqs, p);
 
                                 if(sub != null)
                                 {
@@ -160,6 +188,7 @@ namespace SolveSudoku
 
                                 nr[i].Add(k);
                                 nc[j].Add(k);
+                                nsqs[i / 3, j / 3].Add(k);
                             }
 
                             return null;
--- UnitTestProject/SudokuSolverUnitTest.cs
+++ UnitTestProject/SudokuSolverUnitTest.cs
@@ -10,8 +10,7 @@ namespace UnitTestProject
         [TestMethod]
         public void TestMethod1()
         {
-            int[,] problem = new int[9, 9];
-
+            int[,] problem = null;
             string[] x = new string[] {
                 "060200109",
                 "308007000",
@@ -24,13 +23,7 @@ namespace UnitTestProject
                 "602009050",
             };
 
-            for(int i = 0; i< 9; i++)
-            {
-                for(int j = 0; j < 9; j++)
-                {
-                    problem[i, j] = x[i][j] - '0';
-                }
-            }
+            problem = InitializeProblemFromString(x);
 
             var sb = new System.Text.StringBuilder();
             var tw = new System.IO.StringWriter(sb);
@@ -49,6 +42,57 @@ namespace UnitTestProject
             sb.Clear();
         }
 
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[,] problem = null;
+            string[] x = new string[] {
+                "006020100",
+                "000617000",
+                "710000095",
+                "100352009",
+                "403000502",
+                "900784001",
+                "650000023",
+                "000269000",
+                "009070400",
+            };
+
+            problem = InitializeProblemFromString(x);
+
+            var sb = new System.Text.StringBuilder();
+            var tw = new System.IO.StringWriter(sb);
+            var solver = new SolveSudoku.SudokuSolver();
+            solver.PrintSolution(problem, tw);
+            var result = solver.Solve(problem);
+            Assert.IsNotNull(result);
+
+            foreach (int[,] r in solver.GetAllAnswers())
+            {
+                VerifySolution(r);
+            }
+
+            sb.Clear();
+            tw.Dispose();
+            sb.Clear();
+        }
+
+        private static int[,] InitializeProblemFromString(string[] x)
+        {
+            int[,] problem = new int[9, 9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    problem[i, j] = x[i][j] - '0';
+                }
+            }
+
+            return problem;
+        }
+
         private static void VerifySolution(int[,] result)
         {
             
@@ -56,38 +100,45 @@ namespace UnitTestProject
 
             for (int i = 0; i < 9; i++)
             {
-                List<int> y = new List<int>();
+                List<int> y = new List<int>(), z = new List<int>();
 
                 for (int j = 0; j < 9; j++)
                 {
                     Assert.IsTrue(result[i, j] >= 1 && result[i, j] <= 9);
+                    Assert.IsTrue(result[j, i] >= 1 && result[j, i] <= 9);
                     y.Add(result[i, j]);
+                    z.Add(result[j, i]);
                 }
 
-                y.Sort();
-
-                for (int j = 0; j < 9; j++)
-                {
-                    Assert.IsTrue(y[j] == j + 1);
-                }
+                IsList1To9(y);
+                IsList1To9(z);
             }
 
-            for (int i = 0; i < 9; i++)
+            for(int i = 0; i < 3; i++)
             {
-                List<int> y = new List<int>();
-
-                for (int j = 0; j < 9; j++)
+                for(int j = 0; j < 3; j++)
                 {
-                    Assert.IsTrue(result[j, i] >= 1 && result[j, i] <= 9);
-                    y.Add(result[j, i]);
-                }
+                    List<int> y = new List<int>();
 
-                y.Sort();
-
-                for (int j = 0; j < 9; j++)
-                {
-                    Assert.IsTrue(y[j] == j + 1);
+                    for(int k = 0; k < 3; k++)
+                    {
+                        for(int l = 0; l < 3; l++)
+                        {
+                            y.Add(result[i * 3 + k, j * 3 + l]);
+                        }
+                    }
                 }
+            }
+        }
+
+        private static void IsList1To9(List<int> y)
+        {
+            Assert.AreEqual(9, y.Count);
+            y.Sort();
+
+            for (int j = 0; j < 9; j++)
+            {
+                Assert.IsTrue(y[j] == j + 1);
             }
         }
     }
